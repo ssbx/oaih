8c4f7e1012c1eedff77b0a430d3e16bedf9027fe
Sébastien Serre <sserre.bx@gmail.com>
2017-02-28 12:14:42 +0100

    ok get

--- oaih.c
+++ oaih.c
@@ -1,28 +1,4 @@
 /*
- * The MIT License
- *
- * Copyright 2017 Sébastien Serre <sserre at msha.fr>.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-/*
  * File:   oaih.c
  * Author: Sébastien Serre <sserre at msha.fr>
  *
@@ -32,106 +8,37 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <xml.h>
+#include <clog.h>
 #include <cargo.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <ssock.h>
+
+char HTTP_GET[] =
+        "GET / HTTP/1.0\r\n"
+        "Host: www.example.com\r\n\r\n";
+
+#define PAYLOAD_MAX 100000
 
 /*
  *
  */
-int main(int argc, char** argv) {
-    char buffer[BUFSIZ];
+int
+main(int argc, char** argv) {
+    int fd, count;
+    char payload[PAYLOAD_MAX + 1];
 
-    enum CONSTEXPR {
-        MAX_REQUEST_LEN = 1024
-    };
-    char request[MAX_REQUEST_LEN];
-    char request_template[] = "GET / HTTP/1.1\r\nHost: %s\r\n\r\n";
-    struct protoent *protoent;
-    char *hostname = "example.com";
-    in_addr_t in_addr;
-    int request_len;
-    int socket_file_descriptor;
-    ssize_t nbytes_total, nbytes_last;
-    struct hostent *hostent;
-    struct sockaddr_in sockaddr_in;
-    unsigned short server_port = 80;
-
-    if (argc > 1)
-        hostname = argv[1];
-
-    request_len = snprintf(request, MAX_REQUEST_LEN, request_template, hostname);
-    if (request_len >= MAX_REQUEST_LEN) {
-        fprintf(stderr, "request length large: %d\n", request_len);
-        exit(EXIT_FAILURE);
+    if ((fd = netdial(TCP, "www.example.com", 80)) < 0) {
+        return (EXIT_FAILURE);
     }
 
-    /* Build the socket. */
-    protoent = getprotobyname("tcp");
-    if (protoent == NULL) {
-        perror("getprotobyname");
-        exit(EXIT_FAILURE);
-    }
-    socket_file_descriptor = socket(AF_INET, SOCK_STREAM, protoent->p_proto);
-    if (socket_file_descriptor == -1) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
 
-    /* Build the address. */
-    hostent = gethostbyname(hostname);
-    if (hostent == NULL) {
-        fprintf(stderr, "error: gethostbyname(\"%s\")\n", hostname);
-        exit(EXIT_FAILURE);
-    }
-    in_addr = inet_addr(inet_ntoa(*(struct in_addr*) *(hostent->h_addr_list)));
-    if (in_addr == (in_addr_t) - 1) {
-        fprintf(stderr, "error: inet_addr(\"%s\")\n", *(hostent->h_addr_list));
-        exit(EXIT_FAILURE);
-    }
-    sockaddr_in.sin_addr.s_addr = in_addr;
-    sockaddr_in.sin_family = AF_INET;
-    sockaddr_in.sin_port = htons(server_port);
-
-    /* Actually connect. */
-    if (connect(socket_file_descriptor, (struct sockaddr*) &sockaddr_in, sizeof (sockaddr_in)) == -1) {
-        perror("connect");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Send HTTP request. */
-    nbytes_total = 0;
-    while (nbytes_total < request_len) {
-        nbytes_last = write(socket_file_descriptor, request + nbytes_total, request_len - nbytes_total);
-        if (nbytes_last == -1) {
-            perror("write");
-            exit(EXIT_FAILURE);
+    if (write(fd, HTTP_GET, strlen(HTTP_GET)) >= 0) {
+        while ((count = read(fd, payload, PAYLOAD_MAX)) > 0) {
+            printf("%s", payload);
         }
-        nbytes_total += nbytes_last;
     }
 
-    /*
-    Read the response. TODO: for example.com, the second read hangs for a few seconds.
-    Why? Does the connection keep open until I close it?
-    Must I parse the returned string for Content-Length to see if the HTTP response is over,
-    and close it then? http://stackoverflow.com/a/25586633/895245 says that if Content-Length
-    is not sent, the server can just close to determine length.
-    curl example.com does not lag at all.
-     */
-    fprintf(stderr, "debug: before first read\n");
-    while ((nbytes_total = read(socket_file_descriptor, buffer, BUFSIZ)) > 0) {
-        fprintf(stderr, "debug: after a read\n");
-        write(STDOUT_FILENO, buffer, nbytes_total);
-    }
-    fprintf(stderr, "debug: after last read\n");
-    if (nbytes_total == -1) {
-        perror("read");
-        exit(EXIT_FAILURE);
-    }
-
-    close(socket_file_descriptor);
-    exit(EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
+
